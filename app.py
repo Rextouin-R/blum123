966d84cfdadddf9916592b15ed5f5a63ccb16dd1
Rextouin-R <rattatouille1994@gmail.com>
2024-10-12 04:35:17 +0700

    app.py

--- app.py
+++ app.py
@@ -558,7 +558,14 @@ if __name__ == '__main__':
         queries_files.sort(key=lambda x: int(re.findall(r'\d+', x)[0]) if re.findall(r'\d+', x) else 0)
 
         blum.print_timestamp(
-            f"{Fore.MAGENTA + Style.BRIGHT}{Style.RESET_ALL}"
+            f"{Fore.MAGENTA + Style.BRIGHT}
+            ==============================================
+                  NOT PIXEL BOT | AIRDROP UNLIMITED    
+            ==============================================
+                Telegram Channel : @airdrop_unlimited          
+               Telegram Chat   : @unlimited_discussion        
+            ==============================================
+            {Style.RESET_ALL}"
             
         )
 
