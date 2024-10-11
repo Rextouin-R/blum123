df34bfb13031d5a8194198b3cf96680e9f9b9ae3
Rextouin-R <rattatouille1994@gmail.com>
2024-10-12 04:50:13 +0700

    app.py

--- app.py
+++ app.py
@@ -558,9 +558,21 @@ if __name__ == '__main__':
         queries_files.sort(key=lambda x: int(re.findall(r'\d+', x)[0]) if re.findall(r'\d+', x) else 0)
 
         blum.print_timestamp(
+            f"{Fore.MAGENTA + Style.BRIGHT}[ ===> ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+            f"{Fore.CYAN + Style.BRIGHT}[ REXTOUIN_RR ]{Style.RESET_ALL}"
+        )
+
+        blum.print_timestamp(
             f"{Fore.MAGENTA + Style.BRIGHT}[ JOIN ]{Style.RESET_ALL}"
             f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
             f"{Fore.CYAN + Style.BRIGHT}[ AIRDROP_UNLIMITED ]{Style.RESET_ALL}"
+        )
+
+        blum.print_timestamp(
+            f"{Fore.MAGENTA + Style.BRIGHT}[ JOIN ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+            f"{Fore.CYAN + Style.BRIGHT}[ UNLIMITED_DISCUSSION ]{Style.RESET_ALL}"
         )
 
         blum.print_timestamp(
